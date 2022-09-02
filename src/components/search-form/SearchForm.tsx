d324109d26eae76811bce8bbd21094118f3c1982
benjini123 <benjahenley@hotmail.com>
2022-09-02 15:08:38 -0300

    arreglo import hooks

--- src/components/search-form/SearchForm.tsx
+++ src/components/search-form/SearchForm.tsx
@@ -1,4 +1,4 @@
-import { useSearchResults } from "hooks";
+import { useSearchResults } from "../../hooks";
 import { useNavigate } from "react-router-dom";
 
 export function SearchForm({ style }) {
